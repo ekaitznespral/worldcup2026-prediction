60e5b7718dfb2027550cbf8df93261a91dc7fbd3
ekaitznespral <160833800+ekaitznespral@users.noreply.github.com>
2026-06-11 23:37:18 +0200

    Update results.js
    resultados de grupos comentados

--- results.js
+++ results.js
@@ -33,17 +33,94 @@ const RESULTS = {
        //"South Africa__Czech Republic": { "home": 1, "away": 0 },
        //"South Korea__Czech Republic": { "home": 1, "away": 1 }
     }, 
-    "B": {},
-    "C": {},
-    "D": {},
-    "E": {},
-    "F": {},
-    "G": {},
-    "H": {},
-    "I": {},
-    "J": {},
-    "K": {},
-    "L": {}
+    "B": {
+       //"Canada__Switzerland": { "home": 0, "away": 0 },
+       //"Canada__Qatar": { "home": 2, "away": 1 },
+       //"Canada__Bosnia & Herzegovina": { "home": 1, "away": 1 },
+       //"Switzerland__Qatar": { "home": 2, "away": 0 },
+       //"Switzerland__Bosnia & Herzegovina": { "home": 2, "away": 1 },
+       //"Qatar__Bosnia & Herzegovina": { "home": 1, "away": 0 }
+ },
+ "C": {
+       //"Brazil__Haiti": { "home": 3, "away": 0 },
+       //"Brazil__Morocco": { "home": 2, "away": 0 },
+       //"Brazil__Scotland": { "home": 2, "away": 1 },
+       //"Haiti__Morocco": { "home": 1, "away": 3 },
+       //"Haiti__Scotland": { "home": 1, "away": 3 },
+       //"Morocco__Scotland": { "home": 0, "away": 1 }
+ },
+ "D": {
+       //"Turkey__Paraguay": { "home": 2, "away": 1 },
+       //"Turkey__Australia": { "home": 2, "away": 1 },
+       //"Turkey__USA": { "home": 2, "away": 0 },
+       //"Paraguay__Australia": { "home": 1, "away": 0 },
+       //"Paraguay__USA": { "home": 0, "away": 0 },
+       //"Australia__USA": { "home": 0, "away": 0 }
+ },
+ "E": {
+       //"Curaçao__Ecuador": { "home": 2, "away": 1 },
+       //"Curaçao__Germany": { "home": 1, "away": 0 },
+       //"Curaçao__Ivory Coast": { "home": 1, "away": 3 },
+       //"Ecuador__Germany": { "home": 2, "away": 1 },
+       //"Ecuador__Ivory Coast": { "home": 0, "away": 2 },
+       //"Germany__Ivory Coast": { "home": 0, "away": 1 }
+ },
+ "F": {
+       //"Japan__Netherlands": { "home": 3, "away": 1 },
+       //"Japan__Tunisia": { "home": 1, "away": 1 },
+       //"Japan__Sweden": { "home": 2, "away": 2 },
+       //"Netherlands__Tunisia": { "home": 1, "away": 0 },
+       //"Netherlands__Sweden": { "home": 1, "away": 0 },
+       //"Tunisia__Sweden": { "home": 1, "away": 3 }
+ },
+ "G": {
+       //"Egypt__Belgium": { "home": 2, "away": 1 },
+       //"Egypt__Iran": { "home": 2, "away": 1 },
+       //"Egypt__New Zealand": { "home": 3, "away": 1 },
+       //"Belgium__Iran": { "home": 1, "away": 0 },
+       //"Belgium__New Zealand": { "home": 1, "away": 0 },
+       //"Iran__New Zealand": { "home": 1, "away": 2 }
+ },
+ "H": {
+       //"Uruguay__Spain": { "home": 1, "away": 0 },
+       //"Uruguay__Saudi Arabia": { "home": 3, "away": 1 },
+       //"Uruguay__Cape Verde": { "home": 0, "away": 0 },
+       //"Spain__Saudi Arabia": { "home": 0, "away": 1 },
+       //"Spain__Cape Verde": { "home": 2, "away": 1 },
+       //"Saudi Arabia__Cape Verde": { "home": 0, "away": 1 }
+ },
+ "I": {
+       //"France__Iraq": { "home": 3, "away": 1 },
+       //"France__Senegal": { "home": 3, "away": 2 },
+       //"France__Norway": { "home": 0, "away": 2 },
+       //"Iraq__Senegal": { "home": 2, "away": 1 },
+       //"Iraq__Norway": { "home": 1, "away": 1 },
+       //"Senegal__Norway": { "home": 0, "away": 1 }
+ },
+ "J": {
+       //"Argentina__Jordan": { "home": 1, "away": 3 },
+       //"Argentina__Algeria": { "home": 2, "away": 0 },
+       //"Argentina__Austria": { "home": 1, "away": 0 },
+       //"Jordan__Algeria": { "home": 3, "away": 2 },
+       //"Jordan__Austria": { "home": 1, "away": 1 },
+       //"Algeria__Austria": { "home": 2, "away": 0 }
+ },
+ "K": {
+       //"Portugal__Uzbekistan": { "home": 2, "away": 1 },
+       //"Portugal__DR Congo": { "home": 2, "away": 1 },
+       //"Portugal__Colombia": { "home": 0, "away": 1 },
+       //"Uzbekistan__DR Congo": { "home": 0, "away": 3 },
+       //"Uzbekistan__Colombia": { "home": 2, "away": 2 },
+       //"DR Congo__Colombia": { "home": 1, "away": 0 }
+ },
+ "L": {
+       //"England__Ghana": { "home": 2, "away": 3 },
+       //"England__Croatia": { "home": 0, "away": 1 },
+       //"England__Panama": { "home": 0, "away": 2 },
+       //"Ghana__Croatia": { "home": 2, "away": 0 },
+       //"Ghana__Panama": { "home": 1, "away": 1 },
+       //"Croatia__Panama": { "home": 1, "away": 2 }
+ }
   },
   "knockout": {
     "round32": [],
