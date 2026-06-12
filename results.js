013fdbdf4faaeb30e1df436f7a1fc997b83cc867
ekaitznespral <160833800+ekaitznespral@users.noreply.github.com>
2026-06-12 07:02:42 +0200

    Update results.js
    Coreaaa

--- results.js
+++ results.js
@@ -31,7 +31,7 @@ const RESULTS = {
        //"Mexico__Czech Republic": { "home": 2, "away": 1 },
        //"South Africa__South Korea": { "home": 2, "away": 0 },
        //"South Africa__Czech Republic": { "home": 1, "away": 0 },
-       //"South Korea__Czech Republic": { "home": 1, "away": 1 }
+       "South Korea__Czech Republic": { "home": 2, "away": 1 }
     }, 
     "B": {
        //"Canada__Switzerland": { "home": 0, "away": 0 },
